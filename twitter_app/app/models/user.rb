6b09a75c9cb0ae1763ce5cc0e6d5645b7eadbecc
McNeelands <m.mcneelands@hotmail.com>
2016-04-05 14:12:05 -0400

    working signup/login

--- twitter_app/app/models/user.rb
+++ twitter_app/app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_many :posts
   has_secure_password
   validates :first_name, :email,  presence: true
-  validates :email, :username uniqueness: true
+  validates :email, :username, uniqueness: true
   validates :first_name, length: { minimum: 2 }
 end
