3e5e370b00a92b27e9874d3dd45ab0174b7d772f
McNeelands <m.mcneelands@hotmail.com>
2016-04-05 12:47:01 -0400

    login/signup done

--- twitter_app/app/models/user.rb
+++ twitter_app/app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :posts
   has_secure_password
+  validates :first_name, :email,  presence: true
+  validates :email, :username uniqueness: true
+  validates :first_name, length: { minimum: 2 }
 end
--- twitter_app/config/routes.rb
+++ twitter_app/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :users do
-    resources :posts
-  end
-  root 'users#new'
+  resources :users
+  resources :posts
+
+  root 'sessions#new'
+  get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
