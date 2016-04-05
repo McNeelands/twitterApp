1fc3013312f7b2b588fc3218d46cdfe8715c80d4
McNeelands <m.mcneelands@hotmail.com>
2016-04-05 11:18:09 -0400

    users/signup

--- twitter_app/config/routes.rb
+++ twitter_app/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do
+    resources :posts
+  end
+  root 'users#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
